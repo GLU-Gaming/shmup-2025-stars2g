e6148d391e3fea33368138f36a5d148499799277
SHDA-GitHub <souf.aliah.sa@gmail.com>
2025-03-20 11:10:18 +0100

    finishUI partially done

---
+++ Assets/Scenes/Test Scenes/Devs/Dev Scene Soufiane/Finish line/scripts/Finish.cs
@@ -0,0 +1,34 @@
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Finish : MonoBehaviour
+{
+    public GameObject winUI;
+    public GameObject player;
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            PlayerWin();
+        }
+    }
+
+    public void PlayerWin()
+    {
+        Instantiate(winUI);
+        Time.timeScale = 0f;
+        if (player.TryGetComponent<PlayerMovement>(out var controller))
+        {
+            controller.enabled = false;
+        }
+    }
+
+    public void RestartGame()
+    {
+        Time.timeScale = 1f;
+        UnityEngine.SceneManagement.SceneManager.LoadScene(
+            UnityEngine.SceneManagement.SceneManager.GetActiveScene().buildIndex);
+    }
+}
+
