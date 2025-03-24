377fb273ad35f2cce13319e1e9fdd7fadffd459f
Jalvent <144919638+JalventScientist@users.noreply.github.com>
2025-03-24 09:16:19 +0100

    moquito prefab, health UI fix

---
+++ Assets/Devs/Scripts/Enemies/AdvancedBehavior/Mosquito.cs
@@ -0,0 +1,92 @@
+using System.Collections;
+using UnityEngine;
+using DG.Tweening;
+
+public class Mosquito : MonoBehaviour
+{
+
+    //Uses bee as a base script but changes the base functionality
+
+    [SerializeField] EntryType entryType;
+    [SerializeField] float aimOffsetZ;
+    [SerializeField] EnemyHealth Health;
+
+    [SerializeField] float LookSpeed;
+
+    GameObject Offset;
+    GameObject player;
+
+    //Base Data for locations
+    Vector2 hozAxes = new Vector2(5, 21);
+    Vector2 verticalAxes = new Vector2(-11, 13);
+
+    bool Charging;
+    private enum EntryType
+    {
+        bottom,
+        top,
+        edge
+    }
+
+    private void Start()
+    {
+        Offset = transform.Find("Offset").gameObject;
+        player = GameObject.FindGameObjectWithTag("Player");
+        StartCoroutine(Animate());
+    }
+
+    IEnumerator Animate()
+    {
+        //Entry Animation
+        Offset.transform.localRotation = Quaternion.Euler(0, 0, -180);
+        switch (entryType)
+        {
+            case EntryType.bottom:
+                transform.position = new Vector3(Random.Range(hozAxes.x, hozAxes.y), -15, 0);
+                transform.rotation = Quaternion.Euler(0, 0, -20);
+                float Height = Random.Range(-10, 10);
+                float DistanceToFly = Mathf.Abs(-15 - Height);
+                float RotateTime = (Random.Range(1.5f, 1.7f) * (DistanceToFly / 10));
+                transform.DOMoveY(Height, (Random.Range(1.4f, 1.7f) * (DistanceToFly / 10))).SetEase(Ease.OutBack);
+                transform.DORotate(new Vector3(0, 0, 5), RotateTime).SetEase(Ease.OutBack);
+                yield return new WaitForSeconds(0.4f);
+                Health.invincible = false;
+                yield return new WaitForSeconds(RotateTime -0.4f);
+
+                break;
+            case EntryType.top:
+                transform.position = new Vector3(Random.Range(hozAxes.x, hozAxes.y), 17, 0);
+                break;
+            case EntryType.edge:
+                
+                transform.position = new Vector3(28, Random.Range(verticalAxes.x, verticalAxes.y), 0);
+                transform.rotation = Quaternion.Euler(0, 0, 20);
+                transform.DOMoveX(Random.Range(hozAxes.x, hozAxes.y), 2f).SetEase(Ease.OutBack);
+                transform.DORotate(new Vector3(0, 0, 0), 2f).SetEase(Ease.OutBack);
+                yield return new WaitForSeconds(0.4f);
+                Health.invincible = false;
+                yield return new WaitForSeconds(1.6f);
+                break;
+        }
+        Charging = true;
+        
+        // Calculate the target position beyond the player
+        Vector3 directionToPlayer = (player.transform.position - transform.position).normalized;
+        Vector3 targetPosition = player.transform.position + directionToPlayer * 50f; // Move 5 units beyond the player
+        // Move towards the target position
+        float distanceToTarget = (targetPosition - transform.position).magnitude;
+
+    }
+
+    private void FixedUpdate()
+    {
+        if (Charging)
+        {
+            // Aim Offset towards player on Z axis with configurable offset
+            Vector3 direction = player.transform.position - Offset.transform.position;
+            float angle = Mathf.Atan2(direction.y, direction.x) * Mathf.Rad2Deg;
+            Quaternion targetRotation = Quaternion.Euler(0, 0, angle + aimOffsetZ);
+            Offset.transform.rotation = Quaternion.Slerp(Offset.transform.rotation, targetRotation, Time.deltaTime * LookSpeed);
+        }
+    }
+}
--- Assets/Devs/Scripts/Enemies/ContactDamage.cs
+++ Assets/Devs/Scripts/Enemies/ContactDamage.cs
@@ -2,11 +2,55 @@ using UnityEngine;
 
 public class ContactDamage : MonoBehaviour
 {
+    [SerializeField] bool isContinuous = false; // Allows for continuous contact damage, like on a laser.
+    [SerializeField] float damageToDeal = 10;
+
+    PlayerHealth playerHealth;
+
+
+    bool hit = false;
+    float coolDownTime = 0f;
+
+    private void Start()
+    {
+        playerHealth = GameObject.FindFirstObjectByType<PlayerHealth>();
+    }
+
+    private void Update()
+    {
+        if (hit && coolDownTime <= 0)
+        {
+            playerHealth.DamagePlayer(damageToDeal);
+            coolDownTime = 0.3f;
+        }
+        if (coolDownTime > 0)
+        {
+            coolDownTime -= Time.deltaTime;
+        }
+    }
+
     private void OnTriggerEnter(Collider other)
     {
         if (other.CompareTag("Player"))
         {
-            GameObject.FindFirstObjectByType<PlayerHealth>().DamagePlayer(10);//sinds de player niet direct is gelinkt aan de health.
+            if (!isContinuous)
+            {
+                playerHealth.DamagePlayer(damageToDeal); // Since the player is not directly linked to the health.
+            }
+            else
+            {
+                hit = true;
+                print("Hitting");
+            }
+        }
+    }
+
+    private void OnTriggerExit(Collider other) // Only applied to continuous contact damage
+    {
+        if (other.CompareTag("Player"))
+        {
+            hit = false;
+            print("I |I II I_");
         }
     }
 }
